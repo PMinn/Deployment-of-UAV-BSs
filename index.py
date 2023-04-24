a1b18c90f8ad47b7516f977125bd13175e23e93c
AlanPan0131 <panalan0131@gmail.com>
2023-04-24 22:37:11 +0800

    update

--- index.py
+++ index.py
@@ -17,8 +17,10 @@ def execute(data):
     UAVBSsSet = eng.main(data['ue_size'], matlab.double([[data['rangeOfPositionMin'], data['rangeOfPositionMax']]]), data['r_UAVBS'], data['isCounterClockwise'], data['startAngleOfSpiral'])
     eel.executeFinished() # type: ignore
 
-
-# ue_size, rangeOfPositionMin, rangeOfPositionMax, r_UAVBS, isCounterClockwise, startAngleOfSpiral
+@eel.expose
+def on_close():
+    print("視窗已關閉")
+    # 在這裡進行需要的操作
 
 eel.init("web")
-eel.start("index.html", mode='chrome', cmdline_args=['--kiosk'])
+eel.start("index.html", size=(2000, 1000), port=50505)
