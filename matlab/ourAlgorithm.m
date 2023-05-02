c7410db7e20511ccbe5685e4abc12e60dc888674
AlanPan0131 <panalan0131@gmail.com>
2023-05-02 19:29:59 +0800

    update

--- matlab/ourAlgorithm.m
+++ matlab/ourAlgorithm.m
@@ -3,19 +3,24 @@ function UAVBSsSet = spiralMBSPlacementAlgorithm(isCounterClockwise, locationOfU
     % locationOfUEs: 所有UE的位置 []
     % r_UAVBS: 無人機的涵蓋範圍半徑
     % UAVBSsSet: 所有無人機的位置 []
+    % UAVBSsRange: 所有無人機涵蓋著的的位置 {[] [];}
     % angle: 旋轉排序的起始角度(0~360deg)
 
-    % Initialization
+    % Initialization(修改過)
     uncoveredUEsSet = locationOfUEs;
     UAVBSsSet = [];
+    % m = 1;
     centerUE = [];
 
-    % 演算法第1行
+    % 演算法第1行(修改過)
     [uncoveredBoundaryUEsSet, angle] = findBoundaryUEsSet(isCounterClockwise, uncoveredUEsSet, angle); % 找出邊緣並以逆時針排序
     while ~isempty(uncoveredUEsSet)
-        % 演算法第2行
+        % 演算法第2行(修改過)
         uncoveredInnerUEsSet = setdiff(uncoveredUEsSet, uncoveredBoundaryUEsSet, 'rows');
         centerUE = uncoveredBoundaryUEsSet(1,:);
+        % if m == 1
+        %     centerUE = uncoveredBoundaryUEsSet(1,:);
+        % end
 
         % 演算法第3行
         % 涵蓋邊緣點
@@ -28,15 +33,15 @@ function UAVBSsSet = spiralMBSPlacementAlgorithm(isCounterClockwise, locationOfU
         newPositionOfUAVBS = secondLocalCoverU; % 最後的涵蓋中心 即為UAVBS的座標
         newUEsSet = secondLocalCoverPprio; % 最後的涵蓋範圍
         
-        % 演算法第5行
+        % 演算法第5行(修改過)
         % 更新結果
         UAVBSsSet(size(UAVBSsSet,1)+1,:) = newPositionOfUAVBS;
         uncoveredUEsSet = setdiff(uncoveredUEsSet, newUEsSet, 'rows');
         % m = m+1;
 
-        % 演算第6行
+        % 演算第6行(修改過)
         % 以不更改排序的情況下移除未覆蓋邊緣集合裡已覆蓋的邊緣點
-        commonRows = ismember(uncoveredBoundaryUEsSet, secondLocalCoverPprio, 'rows');
+        commonRows = ismember(uncoveredBoundaryUEsSet, secondLocalCoverPprio,'rows');
         uncoveredBoundaryUEsSet(commonRows,:) = [];
         if ~isempty(uncoveredBoundaryUEsSet)
             centerUE = uncoveredBoundaryUEsSet(1,:);
