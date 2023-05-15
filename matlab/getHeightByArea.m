50711b0db1f7b7d4aa263f7888a81961a0819d32
AlanPan0131 <panalan0131@gmail.com>
2023-05-16 01:29:06 +0800

    update
    牛頓法

--- matlab/getHeightByArea.m
+++ matlab/getHeightByArea.m
@@ -1,39 +1,17 @@
 function height = getHeightByArea(r_UAVBS)
+    syms h
     r = r_UAVBS;
     a = 12.08;
     b = 0.11;
     etaLoS = 1.6;
     etaNLoS = 23;
-    minHeight = 30; % 法定無人機最低高度
-    maxHeight = 120; % 法定無人機最高高度
-    numOfDecimalPlaces = 2; % 算到小數點後幾位
-
-    % 找到最接近的高度
-    differentials = zeros(maxHeight-minHeight+1,1);
-    for i=1:size(differentials,1)
-        h = minHeight-1+i;
-        theta = atan(h/r);
-
-        differentials(i,1) = pi*tan(theta)/(9*log(10)) + (a*b*(etaLoS-etaNLoS)*exp(-b*(180*theta/pi-a)))/((a*exp(-b*(180*theta/pi-a))+1)^2);
-    end
-    [~, index] = min(abs(differentials));
-    height = minHeight-1+index;
-    if (differentials(height-minHeight+1,1)<0 && differentials(height-minHeight,1)>0) || (differentials(height-minHeight+1,1)>0 && differentials(height-minHeight,1)<0)
-        height = height-1;
-    end
-
-    % 高度貼近小數位
-    decimal = 0.1; % 當前的小數位
-
-    for k=1:numOfDecimalPlaces
-        differentials = zeros(11,1);
-        for i=1:11
-            h = height+decimal*(i-1);
-            theta = atan(h/r);
-            differentials(i,1) = pi*tan(theta)/(9*log(10)) + (a*b*(etaLoS-etaNLoS)*exp(-b*(180*theta/pi-a)))/((a*exp(-b*(180*theta/pi-a))+1)^2);
-        end
-        [~, index] = min(abs(differentials));
-        height = height+decimal*(index-1);
-        decimal = decimal/10;
+    
+    theta = atan(h/r);
+    f =  pi*tan(theta)/(9*log(10)) + (a*b*(etaLoS-etaNLoS)*exp(-b*(180*theta/pi-a)))/((a*exp(-b*(180*theta/pi-a))+1)^2);
+    df = diff(f);
+    tolerance = 1e-3;
+    height = r;
+    while abs(double(subs(f,h,height))) > tolerance
+        height = height - double(subs(f,h,height)) / double(subs(df,h,height));
     end
 end
