2f5ff36e47cddadf284ce317d916bbda18b5e306
AlanPan0131 <panalan0131@gmail.com>
2023-07-02 01:18:39 +0800

    update

--- matlab/drawChart.m
+++ matlab/drawChart.m
@@ -4,14 +4,21 @@ function drawChart()
     % 確保輸出的資料夾存在
     checkOutputDir(outputDir); 
 
-    satisfiedRateData = load(outputDir+"/satisfiedRateData.mat").satisfiedRateData;
+    % data = load(outputDir+"/satisfiedRateData.mat").satisfiedRateData;
+    data = load(outputDir+"/fairnessData.mat").fairnessData;
+    xlabelText = "地面使用者的數量";
+    % ylabelText = "使用者滿意度";
+    ylabelText = "公平性";
+    % outputFile = "/satisfiedRate.jpg";
+    outputFile = "/fairness.jpg";
+
     x = 200:200:1000;
     figure
-    plot(x,satisfiedRateData(:,2),'r-o',x,satisfiedRateData(:,1),'b-square',x,satisfiedRateData(:,3),'m-diamond',x,satisfiedRateData(:,4),'g-^','LineWidth',2,'MarkerSize',10);
-    xlabel('地面使用者的數量','FontName','標楷體');
-    ylabel('使用者滿意度','FontName','標楷體');
+    plot(x,data(:,2),'r-o',x,data(:,1),'b-square',x,data(:,3),'m-diamond',x,data(:,4),'g-^','LineWidth',2,'MarkerSize',10);
+    xlabel(xlabelText,'FontName','標楷體');
+    ylabel(ylabelText,'FontName','標楷體');
     h = legend({'本文所提方法','逆時針螺旋','kmeans(本文所提方法之結果)','kmeans(逆時針螺旋)'},'Location','southwest');
     set(h,'FontName','標楷體');
     grid on;
-    exportgraphics(gcf, outputDir+"/satisfiedRate.jpg", 'Resolution', 150, 'BackgroundColor', "#FFFFFF"); % 130
+    exportgraphics(gcf, outputDir+outputFile, 'Resolution', 150, 'BackgroundColor', "#FFFFFF"); % 130
 end
